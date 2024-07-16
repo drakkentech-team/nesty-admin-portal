3415197fc51812af682713452e1e2660db5768c7
xtnoriginal <44991621+xtnoriginal@users.noreply.github.com>
2024-07-16 11:09:03 +0200

    Enhancement-updated size of groups details  pop up

--- src/pages/manageGroups.vue
+++ src/pages/manageGroups.vue
@@ -222,7 +222,7 @@ const handleViewClick = (event) => {
     props: {
       header: 'Group Details',
       style: {
-        width: '90vw',
+        width: '50vw',
       },
       breakpoints:{
         '960px': '75vw',
